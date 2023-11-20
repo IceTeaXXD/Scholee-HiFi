7eeb682118a630d999d17240f16247678b3ce629
henryanandsr <13521004@std.stei.itb.ac.id>
2023-11-20 19:56:18 +0700

    fix : footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -52,7 +52,7 @@ export default function Footer() {
     <Box
       bg={useColorModeValue('gray.50', 'gray.900')}
       color={useColorModeValue('gray.700', 'gray.200')}
-      marginTop="10"
+      marginTop="auto"
     >
       <Container
           as={Stack}
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Footer from './Footer'
-// import Navbar2 from './Navbar2'
 import Navbar from './Navbar'
-import { Box } from '@chakra-ui/react'
+import { Box,Flex } from '@chakra-ui/react'
 
 export default function Layout(props: React.PropsWithChildren) {
   return (
-    <Box minH={'100vh'} minW={'100vw'}>
+    <Flex minH={'100vh'} direction="column" minW="100vw">
       <Navbar />
+      <Box flex="1">
       {props.children}
+      </Box>
       <Footer />
-    </Box>
+    </Flex>
   )
 }
